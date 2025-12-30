73099b42738e5581bb76c7c6d5e4afbdfc598159
ferhugodev60 <ferhugo60dev@gmail.com>
2025-12-30 21:40:27 +0100

    .

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
                 </h3>
                 <p className="text-base">
                     2nd MIAGE master&apos;s student in Amiens (France). I want to be a{" "}
-                    <Code>Frontend developer</Code>.
+                    <Code>FullStack developer</Code>.
                 </p>
             </div>
             <div className="flex-1 max-md:m-auto ml-auto">
